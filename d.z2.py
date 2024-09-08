eefdb2adc4ab000c423ad1e7157c4fb44d993f16
Sasha-Gor32 <sasha051505@gmail.com>
2024-09-08 09:23:06 +0300

    Add files via upload

---
+++ d.z2.py
@@ -0,0 +1,9 @@
+homework = 12
+
+hours = 1.5
+
+name = 'Python'
+
+Time_for_one_homework = hours / homework
+
+print(name,', всего задач:',homework,', затрачено часов:',hours,', среднее время выполнения',Time_for_one_homework, 'часа')
